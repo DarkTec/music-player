8695c23ecaaa485f6ecc2bd87262f21cc51ef9ec
David Hopson <mantle@hotmail.co.uk>
2020-06-18 19:36:37 +0100

    pre-update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import NodeID3 from "node-id3";
+import { Table, Button } from "semantic-ui-react";
+import { sortBy } from "lodash";
 
 const { resolve } = require('path');
 const { readdir } = require('fs').promises;
@@ -20,6 +22,11 @@ async function* getFiles(dir) {
 function App() {
 
     const [files, setFiles] = useState([]);
+    const [playing, setPlaying] = useState();
+    const [audio, setAudio] = useState(new Audio());
+    const [column, setColumn] = useState(null);
+    const [direction, setDirection] = useState(null);
+
 
     useEffect(() => {
         const loc = "C:\\Users\\mantl\\Music\\MusicBee\\Music\\Take That";
@@ -32,37 +39,71 @@ function App() {
                 }
             }
         })()
-
     }, []);
 
+    const handleClick = (tags, index) => {
+        let audioSplit = audio.src.split("/");
+        let audioName = decodeURIComponent(audioSplit[audioSplit.length - 1]);
+
+        let tagSplit = tags.location.split("\\");
+        let tagName = tagSplit[tagSplit.length - 1];
+
+        if (audioName != tagName) {
+            audio.src = tags.location;
+            audio.play();
+            setPlaying(index);
+        }
+
+        if (audioName == tagName) {
+            audio.pause();
+            audio.src = '';
+            setPlaying(null);
+        }
+    }
+
+    const handleSort = (clickedColumn) => {
+        if (column !== clickedColumn) {
+            setColumn(clickedColumn);
+            setDirection('ascending');
+            setFiles(sortBy(files, [clickedColumn]));
+
+            return;
+        }
+        
+        setColumn(clickedColumn);
+        setFiles(files.reverse());
+        setDirection(direction === 'ascending' ? 'descending' : 'ascending');
+
+    };
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <table>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Album</th>
-                            <th>Artist</th>
-                            <th>Title</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {files.map((value, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>
-                                    </td>
-                                    <td>{value.album}</td>
-                                    <td>{value.artist}</td>
-                                    <td>{value.title}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </header>
-        </div>
+        <Table sortable celled inverted>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell></Table.HeaderCell>
+                    <Table.HeaderCell sorted={column === 'album' ? direction : null} onClick={handleSort.bind(this, 'album')}>Album</Table.HeaderCell>
+                    <Table.HeaderCell sorted={column === 'artist' ? direction : null} onClick={handleSort.bind(this, 'artist')}>Artist</Table.HeaderCell>
+                    <Table.HeaderCell sorted={column === 'title' ? direction : null} onClick={handleSort.bind(this, 'title')}>Title</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {files.map((value, index) => {
+                    return (
+                        <Table.Row key={index}>
+                            <Table.Cell>
+                                {playing !== index
+                                    ? <Button content='Play' onClick={handleClick.bind(this, value, index)} />
+                                    : <Button content='Stop' onClick={handleClick.bind(this, value, index)} />
+                                }
+                            </Table.Cell>
+                            <Table.Cell>{value.album}</Table.Cell>
+                            <Table.Cell>{value.artist}</Table.Cell>
+                            <Table.Cell>{value.title}</Table.Cell>
+                        </Table.Row>
+                    )
+                })}
+            </Table.Body>
+        </Table>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import {
+    Container,
+    Dropdown,
+    Menu,
+} from 'semantic-ui-react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Menu fixed='top' inverted>
+            <Container fluid>
+                <Dropdown item simple text='File'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>Quit</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Container>
+        </Menu>
+        <Container inverted="true" fluid style={{ marginTop: '2.85714286em' }}>
+            <App />
+        </Container>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
